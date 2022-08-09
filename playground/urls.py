3de85e494c20eac5cfc75c68424b9afdd05ab0b6
swapnil.mohaniraj <swapnil.mohaniraj@sezzle.com>
2022-08-09 15:39:02 +0530

    testing getAppID function

--- playground/urls.py
+++ playground/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-
-    path('hello/', views.say_hello)
-
+    path('hello/', views.say_hello),
+    path('allApps/', views.getAllApps)
 ]
--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import requests
+import json
+
 # Create your views here.
 
 def say_hello(request):
     return HttpResponse('Hello World')
+
+
+def getAppID(request):
+    allApps = "https://api.steampowered.com/ISteamApps/GetAppList/v0001/"
+    
+    # query URL and get a list of all apps dictionary - keys: appid, name
+    response = requests.get(allApps).json()
+    applist = response['applist']['apps']['app']
+
+    # find the app ID for the game in URL parameter
+    for item in applist:
+        if item['name'] == gameName:
+            return item['appid']
+
+
+
+
+
